b68a3f4e4bfffc4d2a71cc4e1f30ff60a74e05cc
Ethan Stein <gamesniper99@live.com>
2024-02-29 14:56:45 -0500

    Add Login page and update routing***

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,12 +9,17 @@ import Home from "./Pages/Home.jsx";
 import RSOs from "./Pages/RSOs.jsx";
 import Admins from "./Pages/Admins.jsx";
 import Settings from "./Pages/Settings.jsx";
+import Login from "./Pages/Auth/Login.jsx";
+// import Signup from "./Pages/Signup.jsx";
+// hello world
 
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/rsos", element: <RSOs /> },
   { path: "/admins", element: <Admins /> },
-  { path: "settings", element: <Settings /> },
+  { path: "/settings", element: <Settings /> },
+  { path: "/login", element: <Login /> },
+  // { path: "/signup", element: <Signup /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
